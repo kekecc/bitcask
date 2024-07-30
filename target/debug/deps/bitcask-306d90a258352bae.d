03368de92a9a97b2d5d829ed6ed10da3aea955b3
kk <1606295957@qq.com>
2024-07-30 15:18:13 +0800

    feat: initial rust repo

---
+++ target/debug/deps/bitcask-306d90a258352bae.d
@@ -0,0 +1,5 @@
+/Users/kk/Documents/bitcask/target/debug/deps/libbitcask-306d90a258352bae.rmeta: src/main.rs
+
+/Users/kk/Documents/bitcask/target/debug/deps/bitcask-306d90a258352bae.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bitcask-7bc2c9d236201a6c.d
@@ -0,0 +1,5 @@
+/Users/kk/Documents/bitcask/target/debug/deps/libbitcask-7bc2c9d236201a6c.rmeta: src/main.rs
+
+/Users/kk/Documents/bitcask/target/debug/deps/bitcask-7bc2c9d236201a6c.d: src/main.rs
+
+src/main.rs:
